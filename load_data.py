00b24ff95b73c13319141f723183417cddeef029
Alexey Sikora <55078713+sikoraaxd@users.noreply.github.com>
2023-05-12 20:38:54 +0400

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,16 +1,14 @@
-import zipfile
+import os
 import boto3
 
 if __name__ == '__main__':
       s3 = boto3.client('s3',
                         endpoint_url='https://storage.yandexcloud.net',
-                        aws_access_key_id='YCAJEffTHMPCfFn4jBYUDB6oV',
-                        aws_secret_access_key='YCNdxgyAtr7bUzU0iIQeQi9ViIJ7GS-ZdbiR3Fyo')
+                        aws_access_key_id=os.environ['access_key'],
+                        aws_secret_access_key=os.environ['secret'])
 
       bucket_name = 'sikoraaxd-bucket'
 
       response = s3.get_object(Bucket=bucket_name, Key='dataset.rar')
       with open('./dataset.rar', 'wb') as f:
           f.write(response['Body'].read())
-      with zipfile.ZipFile('./dataset.rar', 'r') as zip_ref:
-            zip_ref.extractall('.')
